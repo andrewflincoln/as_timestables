c4bd1f3ce3071ccf336f8607ad0b9fa614fc5d87
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-09-10 12:30:56 -0700

    seekbar code in place

--- app/src/main/java/com/andrewflincoln/timestables/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/timestables/MainActivity.java
@@ -3,6 +3,9 @@ package com.andrewflincoln.timestables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timesTablesSeekBar = findViewById(R.id.timesTablesSeekBar);
+        ListView timesTablesListView = findViewById(R.id.timesTablesListView);
+
+        timesTablesSeekBar.setMax(20);
+        timesTablesSeekBar.setProgress(10);
+
+        timesTablesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = 1;
+                int timesTableNumber;
+
+                if (i < min) {
+                    timesTableNumber = min;
+                } else {
+                    timesTableNumber = i;
+                }
+
+                Log.i("Seekbar Value", Integer.toString(timesTableNumber));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
